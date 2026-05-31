45e8a12ba2e96441ee774434a9a5059429a25ba2
Intan-maharani <mhrnintann@gmail.com>
2026-05-31 14:14:35 +0800

    update ke2

--- Modul 6/triaal.cpp
+++ Modul 6/triaal.cpp
@@ -44,9 +44,88 @@ void Enter() {
 
 
 
-struct Komit {
+struct Comit {
     string id;
     string pesan;
     string penulis;
     string stempelWaktu;
 };
+
+struct Branch{
+    string nama;
+    Comit* daftarComit;        
+    int jumlahComit;
+    int kapasitasComit;
+};
+
+struct Repositori {
+    string nama;
+    Cabang* daftarCabang;      
+    int jumlahCabang;
+    int kapasitasCabang;
+    string namaCabangAktif;
+};
+
+void comitKeCabang(Cabang& cabang, const string& pesanComit, const string& namaPenulis, const string& idHash) {
+    if (cabang.jumlahComit >= cabang.kapasitasComit) {
+        cabang.kapasitasComit = (cabang.kapasitasComit == 0) ? 4 : cabang.kapasitasComit * 2;
+        Comit* ComitBaru = new Comit[cabang.kapasitasComit];
+        for (int i = 0; i < cabang.jumlahComit; i++) {
+            ComitBaru[i] = cabang.daftarComit[i];
+        }
+        delete[] cabang.daftarComit; 
+        cabang.daftarComit = ComitBaru;
+    }
+    
+    cabang.daftarComit[cabang.jumlahComit].id = idHash;
+    cabang.daftarComit[cabang.jumlahComit].pesan = pesanComit;
+    cabang.daftarComit[cabang.jumlahComit].penulis = namaPenulis;
+    cabang.daftarComit[cabang.jumlahComit].stempelWaktu = WaktuSekarang();
+    cabang.jumlahComit++;
+}
+
+
+void tambahCabangKeRepo(Repositori& repo, const string& namaCabangBaru, Cabang* cabangAsal = nullptr) {
+    if (repo.jumlahCabang >= repo.kapasitasCabang) {
+        repo.kapasitasCabang = (repo.kapasitasCabang == 0) ? 2 : repo.kapasitasCabang * 2;
+        Cabang* cabangBaru = new Cabang[repo.kapasitasCabang];
+        for (int i = 0; i < repo.jumlahCabang; i++) {
+            cabangBaru[i] = repo.daftarCabang[i];
+        }
+        delete[] repo.daftarCabang;
+        repo.daftarCabang = cabangBaru;
+    }
+
+    Cabang& cbBaru = repo.daftarCabang[repo.jumlahCabang];
+    cbBaru.name = namaCabangBaru;
+    cbBaru.jumlahComit = 0;
+    cbBaru.kapasitasComit = 4;
+    cbBaru.daftarComit = new Komit[cbBaru.kapasitasComit];
+
+    
+    if (cabangAsal != nullptr) {
+        for (int i = 0; i < cabangAsal->jumlahKomit; i++) {
+            tambahKomitKeCabang(cbBaru, cabangAsal->daftarComit[i].pesan, cabangAsal->daftarComit[i].penulis, cabangAsal->daftarComit[i].id);
+            cbBaru.daftarComit[i].stempelWaktu = cabangAsal->daftarComit[i].stempelWaktu;
+        }
+    }
+    repo.jumlahCabang++;
+}
+
+
+Cabang* ambilCabangAktif(Repositori& repo) {
+    for (int i = 0; i < repo.jumlahCabang; i++) {
+        if (repo.daftarCabang[i].name == repo.namaCabangAktif) {
+            return &repo.daftarCabang[i];
+        }
+    }
+    return nullptr;
+}
+
+
+void bersihkanMemoriRepositori(Repositori& repo) {
+    for (int i = 0; i < repo.jumlahCabang; i++) {
+        delete[] repo.daftarCabang[i].daftarKomit;
+    }
+    delete[] repo.daftarCabang;
+}
